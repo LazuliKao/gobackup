13f21005ecac3561c720b5dd8e957fcb8e1c9a02
LazuliKao <LazuliKao233@outlook.com>
2026-04-06 00:20:53 +0800

    chore(frontend): remove unused route

--- web/app/browser/[[...model]]/page.tsx
+++
@@ -1,9 +0,0 @@
-export function generateStaticParams() {
-  return [{ model: ['__placeholder__'] }];
-}
-
-import BrowserClient from './browser-client';
-
-export default function BrowserPage({ params }: { params: { model?: string[] } }) {
-  return <BrowserClient model={params.model} />;
-}
